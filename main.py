16e99b97c2f05109593e3c2f7ac3402209092de9
Amann <91307961+Amannpy@users.noreply.github.com>
2025-04-17 17:40:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from app import app  # noqa: F401
+import os
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+port = int(os.environ.get("PORT", 5000))  # Render sets this PORT env var
+
+app.run(host="0.0.0.0", port=port, debug=True)
